8dd27c62e01493b619c0087208f41cd4117a2cbd
zhehaodn@163.com <zhehaodn@163.com>
2025-05-08 16:03:35 +0800

    Update CircleTagPlain.vue

--- src/components/CircleTagPlain.vue
+++ src/components/CircleTagPlain.vue
@@ -17,12 +17,7 @@ defineProps({
 <template>
   <div
     class="inline-flex items-center justify-center capitalize leading-none rounded-full"
-<<<<<<< HEAD:src/components/CircleTagPlain.vue
     :class="[large ? 'w-20 h-20 text-lg' : 'w-8 h-8 text-lg']"
->>>>>>> parent of ab164d2 (Shape):src/components/CircleTagPlain.vue
-=======
-    :class="[Circle ? 'w-20 h-20 text-lg' : 'w-15 h-15 text-lg']"
->>>>>>> parent of fd96660 (shape):src/components/ShapeTagPlain.vue
   >
     <BaseIcon
       v-if="icon"
